5d798cc2c18f7512ccf09bb9ce1c15da4718c8a0
johnwjr <johnwjr@localhost>
2023-12-26 23:40:42 -0500

    Added comments to code

--- FlightAppProject/src/BusinessLogic/SessionManager.java
+++ FlightAppProject/src/BusinessLogic/SessionManager.java
@@ -1,9 +1,14 @@
 package BusinessLogic;
 
+// Class is used for managing the session information of the user
 
 public class SessionManager {
+// Username is stored for the user that logged in
     private static String currentUsername;
+    
+// Stores the flight number selected by the user during the session
     private static String selectedFlightNum;
+
 
     public static String getCurrentUsername() {
         return currentUsername;
@@ -12,7 +17,7 @@ public class SessionManager {
     public static void setCurrentUsername(String username) {
         currentUsername = username;
     }
-
+ 
     public static String getSelectedFlightNum() {
         return selectedFlightNum;
     }
@@ -21,6 +26,7 @@ public class SessionManager {
         selectedFlightNum = flightNum;
     }
 
+ // Session is cleared when user logs out
     public static void clearSession() {
         currentUsername = null;
         selectedFlightNum = null;
